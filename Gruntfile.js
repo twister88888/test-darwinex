13c8f92c1d3e84c12617dd10ad525f3ff50fb99e
twister88888 <twister88888@gmail.com>
2017-04-23 14:14:18 +0200

    Added burguer nabvar and fixed somethings todo

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,49 +25,48 @@ module.exports = function (grunt) {
     },
     /* Unifica y minifica archivos .js
     uglify: {
-      build: {
-        src: ['src/static/js/*.js'],
-        dest: 'public/js/script.min.js'
-      }
-   }, */
-    // Copia archivos y carpetas
-    copy: {
-      /* html: {
-        cwd: 'src/html',
-        src: ['*.html'],
-        dest: 'dist/',
-        expand: true
-     }, */
-      css: {
-        cwd: 'src/css',
-        src: ['*.css'],
-        dest: 'dist/css',
-        expand: true
-      }
-     /* ,
-      js: {
-       cwd: 'src/js',
-       src: ['*.js'],
-       dest: 'dist/js',
-       expand: true
-      },
-      img: {
-       cwd: 'src/img',
-       src: ['*'],
-       dest: 'dist/img',
-       expand: true
-    } */
+    build: {
+    src: ['src/static/js/*.js'],
+    dest: 'public/js/script.min.js'
+  }
+}, */
+// Copia archivos y carpetas
+copy: {
+  html: {
+    cwd: 'src/html',
+    src: ['*.html'],
+    dest: 'dist/',
+    expand: true
+  },
+  css: {
+    cwd: 'src/css',
+    src: ['*.css'],
+    dest: 'dist/css',
+    expand: true
+  },
+  js: {
+    cwd: 'src/js',
+    src: ['*.js'],
+    dest: 'dist/js',
+    expand: true
+  },
+  img: {
+    cwd: 'src/img',
+    src: ['*'],
+    dest: 'dist/img',
+    expand: true
+  }
 
-    },
-    // Elimina archivos y carpetas
-    clean: {
-      remove_css: ['src/css/*.*']
-    }
-  })
+},
+// Elimina archivos y carpetas
+clean: {
+  remove_css: ['src/css/*.*']
+}
+})
 
-  // grunt.loadNpmTasks('grunt-contrib-watch')
-  // grunt.loadNpmTasks('grunt-contrib-uglify')
-  // grunt.loadNpmTasks('grunt-contrib-clean')
-  grunt.loadNpmTasks('grunt-contrib-copy')
-  grunt.registerTask('default', ['sass', 'copy'])
+// grunt.loadNpmTasks('grunt-contrib-watch')
+// grunt.loadNpmTasks('grunt-contrib-uglify')
+// grunt.loadNpmTasks('grunt-contrib-clean')
+grunt.loadNpmTasks('grunt-contrib-copy')
+grunt.registerTask('default', ['sass', 'copy'])
 }
